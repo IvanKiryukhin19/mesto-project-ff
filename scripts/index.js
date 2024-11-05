4573d7af485674b95d1c4cfe377c7a9bd5d546f7
Ivan Kiryukhin <kirivn@yandex.ru>
2024-11-05 07:30:51 +0300

    After remarks

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,20 @@ const templateCard = document.querySelector('#card-template').content;
 const placesList=document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(nameValue, sourceValue, descriptionValue) {
+function createCard(nameValue, sourceValue, removedCard) {
   const newCard=templateCard.querySelector('.card').cloneNode(true);
   const cardImage=newCard.querySelector('.card__image');
   const cardTitle=newCard.querySelector('.card__title');
   const buttonBasket=newCard.querySelector('.card__delete-button');
 
   cardImage.setAttribute('src', sourceValue);
-  cardImage.setAttribute('alt', `Это ${descriptionValue} место - ${nameValue}`);
-  cardImage.style.display='block';
+  cardImage.setAttribute('alt', `Это место - ${nameValue}`);
+  //cardImage.style.display='block';
   cardTitle.textContent=nameValue;
 
-  buttonBasket.addEventListener('click', function(event) {removeCard(event)});
+  buttonBasket.addEventListener('click', removedCard);
 
-  return placesList.append(newCard);
+  return newCard;
 }
 
 // @todo: Функция удаления карточки
@@ -31,18 +31,11 @@ function removeCard(evt) {
 /*в задании этого не было, но думаю мои добавки его не нарушают.
 Создал массив равный количеству карточек, заполнил индексами и отсортировал его рандомно, что бы по индексу выводить корточки
 в случайном порядке при загрузке/перезагрузке.
-Создал массив для описания картинок (атрибута alt), который через функцию передается в создаваемую карточку.
-Тоже рандомно, но в отличие от карточек описание может повторяться.
 */
+
 const randomIndexes=new Array(initialCards.length).fill(0).map((item, index) => index);
 randomIndexes.sort(()=>Math.random() - 0.5);
 
-const descriptionPlaces=['красивое','дивное','чудное','прекрасное','удивительное','волшебное'];
-
-function selectDescriptionPlace() {
-  return descriptionPlaces[Math.round(Math.random()*(descriptionPlaces.length-1))];
-}
-
 for (let i of randomIndexes) {
-  createCard(initialCards[i].name, initialCards[i].link, selectDescriptionPlace());
+  placesList.append(createCard(initialCards[i].name, initialCards[i].link, removeCard));
 }
